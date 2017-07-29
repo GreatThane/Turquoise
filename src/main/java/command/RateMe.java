29633ed13340ba7af22ab41f15d1183b43b7c5b3
GreatThane <ep9630@gmail.com>
2017-07-29 17:02:51 -0500

    INitial commit

--- src/main/java/command/RateMe.java
+++ src/main/java/command/RateMe.java
@@ -14,92 +14,57 @@ public class RateMe {
     public static void showRateMe(MessageReceivedEvent event, MessageChannel channel, String msg, Matcher rateMeMatcherMatcher, User author) {
 
         if (!event.getMessage().getMentionedUsers().isEmpty()) {
-            int memberCount = event.getMessage().getMentionedUsers().size();
 
-            for (int current = 0; current <= memberCount; current++) {
-
-                int term = 1;
-
-                int hotness = 0;
-                int cuteness = 0;
-                int sexiness = 0;
-
-                while (term <= 3) {
-
-                    Random r = new Random();
-                    int Low = 1;
-                    int High = 15;
-                    int result = r.nextInt(High - Low) + Low;
-
-                    if (term == 1) {
-                        hotness = result;
-                    } else if (term == 2) {
-                        cuteness = result;
-                    } else if (term == 3) {
-                        sexiness = result;
-                    }
-                    term++;
-                }
-                Color color = Color.LIGHT_GRAY;
-                if (hotness + cuteness + sexiness > 28) {
-                    color = Color.RED;
-
-                } else if (hotness + cuteness + sexiness > 14 && hotness + cuteness + sexiness <= 28) {
-                    color = Color.PINK;
-
-                } else if (hotness + cuteness + sexiness <= 14) {
-                    color = Color.LIGHT_GRAY;
-                }
-                EmbedBuilder embedBuilder = new EmbedBuilder();
-                User target = event.getMessage().getMentionedUsers().get(current);
-                embedBuilder.setAuthor(target.getName(), target.getAvatarUrl(), target.getAvatarUrl());
-                embedBuilder.addField("Hotness", Integer.toString(hotness), true);
-                embedBuilder.addField("Cuteness", Integer.toString(cuteness), true);
-                embedBuilder.addField("Sexiness", Integer.toString(sexiness), true);
-                embedBuilder.setColor(color);
-                channel.sendMessage(embedBuilder.build()).complete();
+            for (User target : event.getMessage().getMentionedUsers()) {
+                rateUser(target, channel);
             }
         } else {
-
-            int term = 1;
-
-            int hotness = 0;
-            int cuteness = 0;
-            int sexiness = 0;
-
-            while (term <= 3) {
-
-                Random r = new Random();
-                int Low = 1;
-                int High = 15;
-                int result = r.nextInt(High - Low) + Low;
-
-                if (term == 1) {
-                    hotness = result;
-                } else if (term == 2) {
-                    cuteness = result;
-                } else if (term == 3) {
-                    sexiness = result;
-                }
-                term++;
-            }
-            Color color = Color.LIGHT_GRAY;
-            if (hotness + cuteness + sexiness > 28) {
-                color = Color.RED;
-
-            } else if (hotness + cuteness + sexiness > 14 && hotness + cuteness + sexiness <= 28) {
-                color = Color.PINK;
-
-            } else if (hotness + cuteness + sexiness <= 14) {
-                color = Color.LIGHT_GRAY;
-            }
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setAuthor(author.getName(), author.getAvatarUrl(), author.getAvatarUrl());
-            embedBuilder.addField("Hotness", Integer.toString(hotness), true);
-            embedBuilder.addField("Cuteness", Integer.toString(cuteness), true);
-            embedBuilder.addField("Sexiness", Integer.toString(sexiness), true);
-            embedBuilder.setColor(color);
-            channel.sendMessage(embedBuilder.build()).complete();
+            rateUser(author, channel);
         }
     }
+
+    private static void rateUser(User target, MessageChannel channel) {
+        int hotness = 0;
+        int cuteness = 0;
+        int sexiness = 0;
+
+        for (int term = 1; term <= 3; term++) {
+            Random r = new Random();
+            int low = 1;
+            int high = 15;
+            int result = r.nextInt(high - low) + low;
+
+            switch (term) {
+                case 1:
+                    hotness = result;
+                    break;
+                case 2:
+                    cuteness = result;
+                    break;
+                case 3:
+                    sexiness = result;
+                    break;
+                default:
+                    //should never get here
+            }
+        }
+        Color color = Color.LIGHT_GRAY;
+        if (hotness + cuteness + sexiness > 28) {
+            color = Color.RED;
+
+        } else if (hotness + cuteness + sexiness > 14 && hotness + cuteness + sexiness <= 28) {
+            color = Color.PINK;
+
+        } else if (hotness + cuteness + sexiness <= 14) {
+            color = Color.LIGHT_GRAY;
+        }
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setAuthor(target.getName(), target.getAvatarUrl(), target.getAvatarUrl());
+        embedBuilder.addField("Hotness", Integer.toString(hotness), true);
+        embedBuilder.addField("Cuteness", Integer.toString(cuteness), true);
+        embedBuilder.addField("Sexiness", Integer.toString(sexiness), true);
+        embedBuilder.setColor(color);
+        channel.sendMessage(embedBuilder.build()).complete();
+    }
+
 }
